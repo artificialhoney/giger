2049c1a14b59bf845e88498f315aee79ae1f149b
Sebastian Krüger <sk@honeymachine.io>
2023-08-09 00:57:21 +0200

    chore: examples

---
+++ examples/scripts/batch/roop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORKING_DIR=$PWD
+DIR=$WORKING_DIR/out/batch
+
+TYPE=${BATCH_TYPE:-gravedigger}
+NAME=${BATCH_NAME:-peter}
+FACE=$WORKING_DIR/examples/assets/img/faces/$NAME.jpg
+
+cd $DIR/$TYPE/$NAME
+for FILE in *; do
+    if [[ ! -f $FILE ]]; then
+        continue
+    fi
+    sd roop --source $FACE --input $FILE --output "$FILE.swapped.png"
+done
+cd -
