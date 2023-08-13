76fb8df81b9cdcdd7760265f3af8f765ef12f0c0
Sebastian Krüger <sk@honeymachine.io>
2023-08-13 19:42:50 +0200

    chore: docs styling

--- docs/conf.py
+++ docs/conf.py
@@ -177,15 +177,15 @@ html_theme_options = {
         "color-highlight-on-target": "#282a36",
         "color-api-name": "#ff7b7b",
         "color-api-pre-name": "#ff7b7b",
-        "color-background-primary": "#282a36",
-        "color-background-secondary": "#282a36",
+        "color-background-primary": "#17151E",
+        "color-background-secondary": "#17151E",
         "color-sidebar-item-background--hover": "#44475a",
         "color-sidebar-item-expander-background--hover": "#44475a",
     },
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": "https://github.com/artificialhoney/giger",
+            "url": "https://github.com/artificialhoney/amix",
             "html": """
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
@@ -199,8 +199,8 @@ html_theme_options = {
 html_show_sphinx = False
 html_title = "giger"
 
-pygments_style = "one-dark"
-pygments_dark_style = "one-dark"
+pygments_style = "dracula"
+pygments_dark_style = "dracula"
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -226,9 +226,13 @@ pygments_dark_style = "one-dark"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    "css/custom.css",
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%Y"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
