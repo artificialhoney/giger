94a0e6fa8e09588c77234f34a3087af09d42541b
Sebastian Krüger <sk@honeymachine.io>
2023-08-14 21:33:36 +0200

    chore: use mock

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,8 @@
+import os
+from unittest.mock import MagicMock
+
 import pytest
+import yaml
 
 from giger.cli import CLI
 from giger.commands.prompt import PromptCommand
@@ -8,8 +12,17 @@ __copyright__ = "Sebastian Krüger"
 __license__ = "MIT"
 
 
-def test_prompt(mocker):
-    """Prompt Tests"""
-    spy = mocker.spy(PromptCommand, "run")
-    CLI().run(["prompt", '"Spawn in a battle"'])
-    spy.assert_called_once()
+def test_prompt(snapshot):
+    """CLI Prompt Tests"""
+    snapshots_dir = os.path.join(os.path.dirname(__file__), "snapshots", "cli")
+    snapshot.snapshot_dir = snapshots_dir
+
+    fixtures = [("basic", ["This is a test"])]
+    for test_name, fixture in fixtures:
+        PromptCommand.run = MagicMock()
+        CLI().run(["prompt"] + fixture)
+
+        snapshot.assert_match(
+            yaml.dump(PromptCommand.run.call_args.args),
+            os.path.join(snapshots_dir, test_name + ".yml.snapshot"),
+        )
