7daf73eb1a635ceb251bc0a269ea4673d10e4083
Sebastian Krüger <sk@honeymachine.io>
2024-04-26 17:14:44 +0200

    feat: ripart style

--- examples/styled/styled.py
+++ examples/styled/styled.py
@@ -14,11 +14,14 @@ from giger.services.roop import RoopService
 _logger = logging.getLogger(__name__)
 
 _negative_prompt = "deformed, missing limbs, amputated, pants, shorts, cat ears, bad anatomy, naked, no clothes, disfigured, poorly drawn face, mutation, mutated, ugly, disgusting, blurry, watermark, watermarked, oversaturated, obese, doubled face, b&w, black and white, sepia, nude, frekles, no masks, duplicate image, blur, paintings, sketches, lowres, monochrome, grayscale, bad anatomy, fat, facing away, looking away, tilted head, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, username, blurry, bad feet, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, easy negative, glasses"
+
 _rosagotica_style = "carbon, platinum, dark, black and red silver, night, autumn, roses and thorns, ruby glow, burning, in Transylvania, epic composition, epic proportion, contrast, vibrant color, volumetric lighting, HD"
 _gothfunk_style = "steel, dark, black, silver, night, spring, gothic and funky, beautiful light, burning, epic composition, epic proportion, contrast, vibrant color, volumetric lighting, HD"
 _giger_style = "in the art of H.R. Giger, masterpiece, detailed focus, dynamic angle, 32k UHD resolution, best quality, professional photography, highly detailed, depth of field"
 _spawn_style = "in the art of Spawn, comic, masterpiece, detailed focus, 32k UHD resolution, best quality, professional photography, highly detailed"
 _mongol_style = "freedom, steel, normad, light, children, sun, spring, flowers, asian, plains, in Mongolia, buddha, epic composition, epic proportion, contrast, vibrant color, volumetric lighting, HD"
+_ripart = "epic composition, epic proportion, contrast, vibrant color, volumetric lighting, HD"
+
 _blank_style = ""
 
 _styles = {
@@ -27,6 +30,7 @@ _styles = {
     "giger": _giger_style,
     "spawn": _spawn_style,
     "mongol": _mongol_style,
+    "ripart": _ripart,
     "blank": _blank_style,
 }
 
